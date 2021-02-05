b92f1b92c66d38840701b8e541dff1214801e157
shima159753 <shimazakib13@gmail.com>
2021-02-05 18:31:36 +0900

    ユーザー修正1

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
-  validates :last_name_kanji, presence: true
-  validates :first_name_kanji, presence: true
-  validates :last_name_kana, presence: true
-  validates :first_name_kana, presence: true
   validates :birth_day, presence: true
 
   with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: '全角文字を使用してください' } do
