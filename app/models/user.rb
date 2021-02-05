a0dac0d2c2bf6b983b3a6a57abcc078f296f4868
shima159753 <shimazakib13@gmail.com>
2021-02-05 14:11:44 +0900

    全角のバリデーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,17 @@ class User < ApplicationRecord
   validates :first_name_kana, presence: true
   validates :birth_day, presence: true
 
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: '全角文字を使用してください' } do
+    validates :last_name_kanji
+    validates :first_name_kanji
+  end
+
+  with_options presence: true, format: { with: /\A[ァ-ヶ]+\z/, message: '全角カナを使用してください' } do
+    validates :last_name_kana
+    validates :first_name_kana
+  end
+
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
+
 end
