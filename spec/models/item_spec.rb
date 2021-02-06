2eb38c655626eca440a374fc27e4aabf39bbbdbd
shima159753 <shimazakib13@gmail.com>
2021-02-06 12:22:56 +0900

    モデル単体テスト追加

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -34,25 +34,50 @@ RSpec.describe Item, type: :model do
       @item.valid?
       expect(@item.errors.full_messages).to include("Category is not a number")
      end
+     it '商品カテゴリーに0が選択されている' do
+      @item.category_id = 0
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Category must be other than 0")
+     end
      it '商品状態が必須であること' do
       @item.state_id = ''
       @item.valid?
       expect(@item.errors.full_messages).to include("State is not a number")
+     end
+     it '商品状態に0が選択されている' do
+      @item.category_id = 0
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Delivery fee must be other than 0")
      end
      it '配達料金負担が必須であること' do
       @item.delivery_fee_id = ''
       @item.valid?
       expect(@item.errors.full_messages).to include("Delivery fee is not a number")
      end
+     it '配達料金負担に0が選択されている' do
+      @item.category_id = 0
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Delivery fee must be other than 0")
+     end
      it '配達エリアが必須であること' do
       @item.delivery_area_id = ''
       @item.valid?
       expect(@item.errors.full_messages).to include("Delivery area is not a number")
      end
+     it '配達エリアに0が選択されている' do
+      @item.category_id = 0
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Delivery area must be other than 0")
+     end
      it '配達日が必須であること' do
       @item.delivery_days_id = ''
       @item.valid?
       expect(@item.errors.full_messages).to include("Delivery days is not a number")
+     end
+     it '配達日に0が選択されている' do
+      @item.category_id = 0
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Delivery days must be other than 0")
      end
      it '料金が必須であること' do
       @item.price = ''
