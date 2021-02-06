c93c33658ac82031faa83c26450c75904652b895
shima159753 <shimazakib13@gmail.com>
2021-02-06 10:39:36 +0900

    商品入力保存実装

--- app/models/delivery_days.rb
+++ app/models/delivery_days.rb
@@ -1,9 +1,9 @@
 class DeliveryDays < ActiveHash::Base
   self.data = [
-    { id: 1, name: '--' },
-    { id: 2, name: '1日〜2日で発送' },
-    { id: 3, name: '3日〜4日で発送' },
-    { id: 4, name: '4日〜7日で発送' },
+    { id: 0, name: '--' },
+    { id: 1, name: '1日〜2日で発送' },
+    { id: 2, name: '3日〜4日で発送' },
+    { id: 3, name: '4日〜7日で発送' },
   ]
   
    include ActiveHash::Associations
