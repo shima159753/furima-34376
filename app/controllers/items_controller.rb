e81548e2381c1c5cbbb15f9840a39186cb6dbed2
shima159753 <shimazakib13@gmail.com>
2021-02-07 22:40:17 +0900

    コントローラー設定、アクセス制限

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :move_to_index, only: [:edit]
   
   def index
     @items = Item.all.order('created_at DESC')
@@ -22,10 +23,32 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def edit
+      @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path(@item.id)
+    else
+      render :edit
+    end
+
+  end
+
+
   private
 
   def item_params
     params.require(:item).permit(:name, :text, :category_id, :state_id, :delivery_fee_id, :delivery_area_id, :delivery_days_id,
                                  :price, :image).merge(user_id: current_user.id)
   end
+
+  def move_to_index
+    unless current_user.id == Item.find(params[:id]).user.id
+      redirect_to action: :index
+    end
+  end
+
 end
