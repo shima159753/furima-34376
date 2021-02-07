3972747ff3b116699137eade59d0bcf61d5ec555
shima159753 <shimazakib13@gmail.com>
2021-02-07 19:25:07 +0900

    データベースカラム変更修正

--- db/migrate/20210205041900_rename_bitth_day_column_to_users.rb
+++ db/migrate/20210205041900_rename_bitth_day_column_to_users.rb
@@ -1,5 +1,5 @@
 class RenameBitthDayColumnToUsers < ActiveRecord::Migration[6.0]
   def change
-    rename_column :users, :bitth_day, :birth_day
+    rename_column :users, :birth_day, :birth_day
   end
 end
