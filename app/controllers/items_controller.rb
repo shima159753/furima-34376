2e7247154f9ac8f431a946dedc3210b74e78ca7d
shima159753 <shimazakib13@gmail.com>
2021-02-08 11:20:56 +0900

    コントローラー設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :set_item, only: [:edit, :show, :update]
+  before_action :set_item, only: [:edit, :show, :update, :destroy]
   before_action :authenticate_user!, except: [:index, :show]
   before_action :move_to_index, only: [:edit]
 
@@ -33,6 +33,13 @@ class ItemsController < ApplicationController
       redirect_to item_path(@item.id)
     else
       render :edit
+    end
+  end
+
+  def destroy
+    if user_signed_in?
+    @item.destroy
+    redirect_to root_path
     end
   end
 
