0cd9ebe4581eb1a7db7106d9e82957c900dca5b5
shima159753 <shimazakib13@gmail.com>
2021-02-10 12:31:27 +0900

    モデルの単体テスト

--- app/models/order_buyer.rb
+++ app/models/order_buyer.rb
@@ -1,21 +1,16 @@
 class OrderBuyer
   include ActiveModel::Model
   attr_accessor :user_id, :item_id, :postal_code, :delivery_area_id, :city, :address, :building_name, :tel, :order_id, :token
-  
-  #:user_id, :item_id,, :order_id,
 
   with_options presence: true do
-    validates  :city, :address, :tel, :user_id, :item_id, :token
+    validates  :city, :address, :user_id, :item_id, :token
     validates :postal_code, format: {with: /\A\d{3}[-]\d{4}\z/, message: "is invalid. Include hyphen(-)"}
     validates :tel, format: {with: /\A\d{10,11}\z/, message: "is invalid."}
   end
   validates :delivery_area_id, numericality: {other_than: 0, message: "can't be blank"}
-  # ここにバリデーションの処理を書く
 
   def save
     order = Order.create(user_id: user_id, item_id: item_id)
     Buyer.create(postal_code: postal_code, delivery_area_id: delivery_area_id, city: city, address: address, building_name: building_name, tel: tel,order_id: order.id)
   end
 end
-
-# order_id: order.id,
---
+++ spec/models/order_buyer_spec.rb
@@ -0,0 +1,80 @@
+require 'rails_helper'
+
+RSpec.describe OrderBuyer, type: :model do
+describe '商品購入' do
+  before do
+    user = FactoryBot.create(:user) 
+    item = FactoryBot.create(:item) 
+    @order_buyer = FactoryBot.build(:order_buyer,user_id: user.id,item_id: item.id)
+  end
+
+    context '購入できるとき' do
+      it '全ての項目が確認できれば購入できる' do
+        expect(@order_buyer).to be_valid
+      end
+
+      it 'building_nameは空でも保存できること' do
+        @order_buyer.building_name = ""
+        expect(@order_buyer).to be_valid
+      end
+    end
+
+    context '購入できないとき' do
+      it 'tokenが空だと保存できないこと' do
+        @order_buyer.token = nil
+        @order_buyer.valid?
+        expect(@order_buyer.errors.full_messages).to include("Token can't be blank")
+      end
+      it 'postal_codeが空だと保存できないこと' do
+        @order_buyer.postal_code = nil
+        @order_buyer.valid?
+        expect(@order_buyer.errors.full_messages).to include("Postal code can't be blank")
+      end
+
+      it 'postal_codeがにハイフンがないと保存できないこと' do
+        @order_buyer.postal_code = "1234567"
+        @order_buyer.valid?
+        expect(@order_buyer.errors.full_messages).to include("Postal code is invalid. Include hyphen(-)")
+      end
+
+      it 'delivery_area_idが0だと保存できないこと' do
+        @order_buyer.delivery_area_id = 0
+        @order_buyer.valid?
+        expect(@order_buyer.errors.full_messages).to include("Delivery area can't be blank")
+      end
+
+      it 'cityが空だと保存できないこと' do
+        @order_buyer.city = nil
+        @order_buyer.valid?
+        expect(@order_buyer.errors.full_messages).to include("City can't be blank")
+      end
+     
+      it 'addressが空だと保存できないこと' do
+        @order_buyer.address = nil
+        @order_buyer.valid?
+        expect(@order_buyer.errors.full_messages).to include("Address can't be blank")
+      end
+
+
+
+      it 'telが空だと保存できないこと' do
+        @order_buyer.tel = nil
+        @order_buyer.valid?
+        expect(@order_buyer.errors.full_messages).to include("Tel can't be blank")
+      end
+
+      it 'telが10桁か11桁の数字でないと保存できないこと' do
+        @order_buyer.tel = "333"
+        @order_buyer.valid?
+        expect(@order_buyer.errors.full_messages).to include("Tel is invalid.")
+      end
+
+      it 'telが半角数字でないと保存できないこと' do
+        @order_buyer.tel = "あああああaaaaaa"
+        @order_buyer.valid?
+        expect(@order_buyer.errors.full_messages).to include("Tel is invalid.")
+      end
+
+    end
+  end
+end
